bb30de5910cc45354fbd46b455370bdd702218db
Isabela <isabela.r2005@aluno.ifsc.edu.br>
2022-08-18 12:04:11 -0300

    Tela login

--- src/visao/TelaLoginAdm.java
+++ src/visao/TelaLoginAdm.java
@@ -1,12 +1,22 @@
 package visao;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaLoginAdm {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -17,6 +27,9 @@ public class TelaLoginAdm {
 				try {
 					TelaLoginAdm window = new TelaLoginAdm();
 					window.frame.setVisible(true);
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setExtendedState(window.frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,8 +49,61 @@ public class TelaLoginAdm {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.getContentPane().setBackground(new Color(153, 204, 153));
+		frame.getContentPane().setLayout(null);
+
+		JLabel lblAdm = new JLabel("Administrador");
+		lblAdm.setForeground(Color.WHITE);
+		lblAdm.setBounds(423, 81, 719, 149);
+		lblAdm.setFont(new Font("DejaVu Sans Light", Font.PLAIN, 99));
+		frame.getContentPane().add(lblAdm);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(445, 312, 655, 445);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(214, 87, 46, 14);
+		panel_1.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(92, 122, 433, 20);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(202, 191, 46, 14);
+		panel_1.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(92, 211, 433, 20);
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(92, 258, 46, 14);
+		panel_1.add(lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(51, 102, 51));
+		btnNewButton.setFont(new Font("Alef", Font.PLAIN, 25));
+		btnNewButton.setBounds(124, 307, 375, 46);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cadastrar");
+		btnNewButton_1.setForeground(new Color(51, 102, 51));
+		btnNewButton_1.setBackground(new Color(255, 255, 255));
+		btnNewButton_1.setFont(new Font("Alef", Font.PLAIN, 25));
+		btnNewButton_1.setBounds(124, 364, 375, 45);
+		panel_1.add(btnNewButton_1);
+		frame.setBounds(100, 100, 1600, 850);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
