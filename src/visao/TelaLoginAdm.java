8f59a66da17ba78f2207cdb972e46f6edad637cd
ireinhold <isabela.r2005@aluno.ifsc.edu.br>
2022-08-25 12:03:45 -0300

    Tela Login 25/08
    Arrumar imagem

--- src/visao/TelaLoginAdm.java
+++ src/visao/TelaLoginAdm.java
@@ -10,13 +10,22 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.event.ActionEvent;
+import java.awt.Rectangle;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.SwingConstants;
 
 public class TelaLoginAdm {
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JFrame frmLoginDoAdministrador;
+	private JTextField txtUsuarioAdm;
+	private JTextField txtSenhaAdm;
 
 	/**
 	 * Launch the application.
@@ -26,9 +35,9 @@ public class TelaLoginAdm {
 			public void run() {
 				try {
 					TelaLoginAdm window = new TelaLoginAdm();
-					window.frame.setVisible(true);
-					window.frame.setLocationRelativeTo(null);
-					window.frame.setExtendedState(window.frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+					window.frmLoginDoAdministrador.setVisible(true);
+					window.frmLoginDoAdministrador.setLocationRelativeTo(null);
+					window.frmLoginDoAdministrador.setExtendedState(window.frmLoginDoAdministrador.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -48,73 +57,83 @@ public class TelaLoginAdm {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.getContentPane().setBackground(new Color(153, 204, 153));
-		frame.getContentPane().setLayout(null);
+		frmLoginDoAdministrador = new JFrame();
+		frmLoginDoAdministrador.setResizable(false);
+		frmLoginDoAdministrador.setTitle("Login do Administrador");
+		frmLoginDoAdministrador.getContentPane().setBackground(new Color(150, 191, 120));
+		frmLoginDoAdministrador.getContentPane().setLayout(null);
 
 		JLabel lblAdm = new JLabel("Administrador");
-		lblAdm.setForeground(Color.WHITE);
-		lblAdm.setBounds(423, 51, 719, 149);
-		lblAdm.setFont(new Font("DejaVu Sans Light", Font.PLAIN, 99));
-		frame.getContentPane().add(lblAdm);
+		lblAdm.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdm.setForeground(new Color(234, 242, 237));
+		lblAdm.setBounds(432, 50, 719, 149);
+		lblAdm.setFont(new Font("Dialog", Font.PLAIN, 100));
+		frmLoginDoAdministrador.getContentPane().add(lblAdm);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(445, 312, 655, 445);
-		frame.getContentPane().add(panel_1);
+		JPanel panel_1 =  new JPanel();
+		panel_1.setBackground(new Color(234, 242, 237));
+		panel_1.setBounds(466, 315, 651, 450);
+		frmLoginDoAdministrador.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usu\u00E1rio");
-		lblNewLabel.setForeground(new Color(0, 51, 0));
+		lblNewLabel.setForeground(new Color(31, 65, 45));
 		lblNewLabel.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 27));
 		lblNewLabel.setBounds(37, 38, 146, 31);
 		panel_1.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBackground(Color.WHITE);
-		textField.setForeground(Color.DARK_GRAY);
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		textField.setBounds(37, 80, 578, 39);
-		panel_1.add(textField);
-		textField.setColumns(10);
+		txtUsuarioAdm = new JTextField();
+		txtUsuarioAdm.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
+		txtUsuarioAdm.setBackground(Color.WHITE);
+		txtUsuarioAdm.setForeground(Color.DARK_GRAY);
+		txtUsuarioAdm.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		txtUsuarioAdm.setBounds(36, 80, 578, 39);
+		panel_1.add(txtUsuarioAdm);
+		txtUsuarioAdm.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Senha");
 		lblNewLabel_1.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 27));
-		lblNewLabel_1.setForeground(new Color(0, 51, 0));
+		lblNewLabel_1.setForeground(new Color(31, 65, 45));
 		lblNewLabel_1.setBackground(new Color(240, 240, 240));
 		lblNewLabel_1.setBounds(37, 150, 120, 31);
 		panel_1.add(lblNewLabel_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		textField_1.setBounds(37, 192, 578, 39);
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
+		txtSenhaAdm = new JTextField();
+		txtSenhaAdm.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
+		txtSenhaAdm.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		txtSenhaAdm.setBounds(36, 192, 578, 39);
+		panel_1.add(txtSenhaAdm);
+		txtSenhaAdm.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("*para logar insira seu e-mail ou CNPJ e a sua senha");
 		lblNewLabel_2.setForeground(Color.DARK_GRAY);
 		lblNewLabel_2.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 16));
-		lblNewLabel_2.setBounds(37, 257, 507, 25);
+		lblNewLabel_2.setBounds(37, 242, 507, 25);
 		panel_1.add(lblNewLabel_2);
 		
-		JButton btnNewButton = new JButton("Login");
+		JButton btnNewButton = new JButton("Entrar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBackground(new Color(0, 51, 0));
+		btnNewButton.setForeground(new Color(234, 242, 237));
+		btnNewButton.setBackground(new Color(31, 65, 45));
 		btnNewButton.setFont(new Font("Alef", Font.PLAIN, 25));
-		btnNewButton.setBounds(124, 307, 375, 46);
+		btnNewButton.setBounds(138, 307, 375, 46);
 		panel_1.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cadastrar");
-		btnNewButton_1.setForeground(new Color(0, 51, 0));
+		btnNewButton_1.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setForeground(new Color(31, 65, 45));
 		btnNewButton_1.setBackground(new Color(255, 255, 255));
 		btnNewButton_1.setFont(new Font("Alef", Font.PLAIN, 25));
-		btnNewButton_1.setBounds(124, 364, 375, 45);
+		btnNewButton_1.setBounds(138, 364, 375, 45);
 		panel_1.add(btnNewButton_1);
-		frame.setBounds(100, 100, 1600, 850);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLoginDoAdministrador.setBounds(100, 100, 1600, 850);
+		frmLoginDoAdministrador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
---
+++ src/visao/TelaLoginVendedor.java
@@ -0,0 +1,134 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JTextField;
+import java.awt.SystemColor;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+
+public class TelaLoginVendedor {
+
+	private JFrame frame;
+	private JTextField txtUsuarioVendedor;
+	private JTextField txtSenhaVendedor;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLoginVendedor window = new TelaLoginVendedor();
+					window.frame.setVisible(true);
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setExtendedState(window.frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaLoginVendedor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(150, 191, 120));
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(234, 242, 237));
+		panel.setBounds(466, 315, 650, 380);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuário");
+		lblUsuario.setForeground(new Color(31, 65, 45));
+		lblUsuario.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 27));
+		lblUsuario.setBounds(37, 37, 146, 31);
+		panel.add(lblUsuario);
+		
+		txtUsuarioVendedor = new JTextField();
+		txtUsuarioVendedor.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
+		txtUsuarioVendedor.setForeground(Color.DARK_GRAY);
+		txtUsuarioVendedor.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		txtUsuarioVendedor.setColumns(10);
+		txtUsuarioVendedor.setBackground(Color.WHITE);
+		txtUsuarioVendedor.setBounds(36, 79, 578, 39);
+		panel.add(txtUsuarioVendedor);
+		
+		txtSenhaVendedor = new JTextField();
+		txtSenhaVendedor.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
+		txtSenhaVendedor.setForeground(Color.DARK_GRAY);
+		txtSenhaVendedor.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		txtSenhaVendedor.setColumns(10);
+		txtSenhaVendedor.setBackground(Color.WHITE);
+		txtSenhaVendedor.setBounds(36, 192, 578, 39);
+		panel.add(txtSenhaVendedor);
+		
+		JLabel lblNewLabel_1 = new JLabel("Senha");
+		lblNewLabel_1.setForeground(new Color(31, 65, 45));
+		lblNewLabel_1.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 27));
+		lblNewLabel_1.setBackground(SystemColor.menu);
+		lblNewLabel_1.setBounds(37, 150, 120, 31);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("*para logar insira seu e-mail ou CNPJ e a sua senha");
+		lblNewLabel_2.setForeground(Color.DARK_GRAY);
+		lblNewLabel_2.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(37, 242, 507, 25);
+		panel.add(lblNewLabel_2);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEntrar.setForeground(new Color(234, 242, 237));
+		btnEntrar.setFont(new Font("Alef", Font.PLAIN, 25));
+		btnEntrar.setBackground(new Color(31, 65, 45));
+		btnEntrar.setBounds(137, 307, 375, 46);
+		panel.add(btnEntrar);
+		
+		JLabel lblVendedor = new JLabel("Vendedor");
+		lblVendedor.setHorizontalAlignment(SwingConstants.CENTER);
+		lblVendedor.setForeground(new Color(234, 242, 237));
+		lblVendedor.setFont(new Font("Dialog", Font.PLAIN, 99));
+		lblVendedor.setBounds(432, 50, 719, 149);
+		frame.getContentPane().add(lblVendedor);
+		
+		JLabel lblNewLabel = new JLabel("");
+		Image img1 = new ImageIcon(this.getClass().getResource("/adm_login.png")).getImage();
+		lblNewLabel.setBounds(776, 290, 46, 14);
+		lblNewLabel.setIcon(new ImageIcon(img1));
+		frame.getContentPane().add(lblNewLabel);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 1600, 850);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
