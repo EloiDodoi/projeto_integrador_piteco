0d7a8a1e86a7cec04095d5bf3c8a48016bbaf458
eloi <isadora.js23@aluno.ifsc.edu.br>
2022-09-01 11:55:00 -0300

    AA

--- src/controle/LoginAdmBD.java
+++ src/controle/LoginAdmBD.java
@@ -1,5 +1,32 @@
 package controle;
 
-public class LoginAdmBD {
+import java.sql.*;
 
+public class LoginAdmBD {
+	
+	
+	
+	
+	Connection conexao;
+	public LoginAdmBD() throws SQLException {
+		conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/piteco", "root", "aluno");
+	}
+	
+	
+	public boolean autenticar() {
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/piteco", "root", "aluno");
+			System.out.println("Conectado!");
+		}catch (SQLException e) {
+			System.out.println("Erro na conexão!");
+		}
+	}
 }
