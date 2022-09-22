0cbe19545b8762ec446e90cc88a69d4b26db5564
eloi <isadora.js23@aluno.ifsc.edu.br>
2022-09-22 07:32:08 -0300

    Senha da conexão

--- src/controle/ConexaoBD.java
+++ src/controle/ConexaoBD.java
@@ -8,7 +8,7 @@ public class ConexaoBD {
 	private Connection conexao;
 
 	public Connection getConexao() throws SQLException {
-		conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/piteco", "root", "Artcev.01");
+		conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/piteco", "root", "aluno");
 		return conexao;
 	}
 	public static Connection fecharConexao(Connection conexao) throws SQLException {
