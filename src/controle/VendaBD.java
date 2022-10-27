9eb84b621723ef1f32c9da9cf6e496fd8a0bd362
Elói Schumacker <isadora.js23@aluno.ifsc.edu.br>
2022-10-27 10:01:15 -0300

    Inicio vendas pt (1)3 :(

--- src/controle/VendaBD.java
+++ src/controle/VendaBD.java
@@ -1,6 +1,31 @@
 package controle;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import modelo.Produto;
+
 public class VendaBD {
 	private ConexaoBD cbd = new ConexaoBD();
-
+	
+	public float precoUnitario(Produto produto, float quantidade) {
+		float preco = 0;
+		try {
+			
+			PreparedStatement ps = cbd.getConexao().prepareStatement("SELECT produto_preco FROM PRODUTO WHERE idProduto = ?");
+			
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				preco = rs.getFloat(1);
+			}
+			return preco * quantidade;
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro em adicionar item à compra!");
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
--- src/modelo/Venda.java
+++ src/modelo/Venda.java
@@ -3,34 +3,43 @@ package modelo;
 import java.sql.Date;
 
 public class Venda {
+
 	private int idVenda;
 	private float venda_valor;
 	private Date venda_data;
-	private int Usuario_idUsuario;
+	private Usuario usuario;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
 	public int getIdVenda() {
 		return idVenda;
 	}
+
 	public void setIdVenda(int idVenda) {
 		this.idVenda = idVenda;
 	}
+
 	public float getVenda_valor() {
 		return venda_valor;
 	}
+
 	public void setVenda_valor(float venda_valor) {
 		this.venda_valor = venda_valor;
 	}
+
 	public Date getVenda_data() {
 		return venda_data;
 	}
+
 	public void setVenda_data(Date venda_data) {
 		this.venda_data = venda_data;
 	}
-	public int getUsuario_idUsuario() {
-		return Usuario_idUsuario;
-	}
-	public void setUsuario_idUsuario(int usuario_idUsuario) {
-		Usuario_idUsuario = usuario_idUsuario;
-	}
-	
-	
+
 }
