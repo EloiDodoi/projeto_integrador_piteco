899c19ced8a050d4794e792cbfa3864a475def98
IReinhold <isabela.r2005@aluno.ifsc.edu.br>
2022-11-22 18:01:29 -0300

    login no main deu certo amem

--- src/visao/TelaLoginAdm.java
+++ src/visao/TelaLoginAdm.java
@@ -1,4 +1,5 @@
 package visao;
+
 import controle.LoginAdmBD;
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -30,14 +31,16 @@ import javax.swing.JPasswordField;
 import javax.swing.ImageIcon;
 import visao.TelaBemVindo;
 import javax.swing.border.EmptyBorder;
+
 public class TelaLoginAdm {
 
 	private JFrame frmLoginDoAdministrador;
 	private JTextField txt_login;
 	private JPasswordField txt_senha;
 	static TelaLoginAdm window = new TelaLoginAdm();
-	
+
 	static Usuario user;
+
 	/**
 	 * Launch the application.
 	 */
@@ -48,7 +51,8 @@ public class TelaLoginAdm {
 					TelaLoginAdm window = new TelaLoginAdm();
 					window.frmLoginDoAdministrador.setVisible(true);
 					window.frmLoginDoAdministrador.setLocationRelativeTo(null);
-					window.frmLoginDoAdministrador.setExtendedState(window.frmLoginDoAdministrador.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+					window.frmLoginDoAdministrador.setExtendedState(
+							window.frmLoginDoAdministrador.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -65,25 +69,25 @@ public class TelaLoginAdm {
 	}
 
 	public void abrir() {
-		frmLoginDoAdministrador.setVisible(true);	
+		frmLoginDoAdministrador.setVisible(true);
 		frmLoginDoAdministrador.setLocationRelativeTo(null);
 		frmLoginDoAdministrador.setExtendedState(frmLoginDoAdministrador.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 	}
-	
+
 	private void initialize() {
 		frmLoginDoAdministrador = new JFrame();
 		frmLoginDoAdministrador.setResizable(false);
 		frmLoginDoAdministrador.setTitle("Login do Administrador");
 		frmLoginDoAdministrador.getContentPane().setBackground(new Color(150, 191, 120));
 		frmLoginDoAdministrador.getContentPane().setLayout(null);
-		
-				JLabel lblAdm = new JLabel("Administrador");
-				lblAdm.setHorizontalAlignment(SwingConstants.CENTER);
-				lblAdm.setForeground(new Color(234, 242, 237));
-				lblAdm.setBounds(325, 50, 719, 149);
-				lblAdm.setFont(new Font("Dialog", Font.PLAIN, 100));
-				frmLoginDoAdministrador.getContentPane().add(lblAdm);
-		
+
+		JLabel lblAdm = new JLabel("Administrador");
+		lblAdm.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdm.setForeground(new Color(234, 242, 237));
+		lblAdm.setBounds(325, 50, 719, 149);
+		lblAdm.setFont(new Font("Dialog", Font.PLAIN, 100));
+		frmLoginDoAdministrador.getContentPane().add(lblAdm);
+
 		JLabel image = new JLabel("");
 		image.setBackground(new Color(255, 255, 255));
 		image.setForeground(new Color(255, 255, 255));
@@ -91,19 +95,19 @@ public class TelaLoginAdm {
 		image.setBounds(595, 189, 180, 180);
 		frmLoginDoAdministrador.getContentPane().add(image);
 
-		JPanel panel_1 =  new JPanel();
+		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(null);
 		panel_1.setBackground(new Color(234, 242, 237));
 		panel_1.setBounds(360, 315, 650, 450);
 		frmLoginDoAdministrador.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Usu\u00E1rio");
 		lblNewLabel.setForeground(new Color(31, 65, 45));
 		lblNewLabel.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 27));
 		lblNewLabel.setBounds(37, 38, 146, 31);
 		panel_1.add(lblNewLabel);
-		
+
 		txt_login = new JTextField();
 		txt_login.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
 		txt_login.setBackground(Color.WHITE);
@@ -112,51 +116,47 @@ public class TelaLoginAdm {
 		txt_login.setBounds(36, 80, 578, 39);
 		panel_1.add(txt_login);
 		txt_login.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Senha");
 		lblNewLabel_1.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 27));
 		lblNewLabel_1.setForeground(new Color(31, 65, 45));
 		lblNewLabel_1.setBackground(new Color(240, 240, 240));
 		lblNewLabel_1.setBounds(37, 150, 120, 31);
 		panel_1.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("*para logar insira seu e-mail ou CNPJ e a sua senha");
 		lblNewLabel_2.setForeground(Color.DARK_GRAY);
 		lblNewLabel_2.setFont(new Font("Lucida Sans Unicode", Font.PLAIN, 16));
 		lblNewLabel_2.setBounds(37, 242, 507, 25);
 		panel_1.add(lblNewLabel_2);
-		
+
 		JButton btn_entrar_adm = new JButton("Entrar");
 		btn_entrar_adm.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
 		btn_entrar_adm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Usuario login = new Usuario(0, null, null, null, null, null, txt_senha.getText(), txt_login.getText(), 0);
+				Usuario login = new Usuario(0, null, null, null, null, null, txt_senha.getText(), txt_login.getText(),
+						0);
 				LoginAdmBD lg;
-				try {
-					lg = new LoginAdmBD();
-					if (lg.autenticar(login)== null ) {
-						System.out.println("Erro Login");
-						JOptionPane.showMessageDialog(null, "E-mail ou senha incorretos, tente novamente!");
-					}else {
-						System.out.println("Login feito com êxito");
-						TelaBemVindo tb = new TelaBemVindo();
-						user = lg.autenticar(login);
-						tb.abrir();
-						frmLoginDoAdministrador.setVisible(false);
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					JOptionPane.showMessageDialog(null, "Erro ao acessar o banco.");
+				lg = new LoginAdmBD();
+				if (lg.autenticar(login) == null) {
+					System.out.println("Erro Login");
+					JOptionPane.showMessageDialog(null, "E-mail ou senha incorretos, tente novamente!");
+				} else {
+					System.out.println("Login feito com êxito");
+					TelaBemVindo tb = new TelaBemVindo();
+					user = lg.autenticar(login);
+					tb.abrir();
+					frmLoginDoAdministrador.setVisible(false);
 				}
-				
-				
-			}});
+
+			}
+		});
 		btn_entrar_adm.setForeground(new Color(234, 242, 237));
 		btn_entrar_adm.setBackground(new Color(31, 65, 45));
 		btn_entrar_adm.setFont(new Font("Alef", Font.PLAIN, 25));
 		btn_entrar_adm.setBounds(138, 307, 375, 46);
 		panel_1.add(btn_entrar_adm);
-		
+
 		JButton btn_cadastrar_adm = new JButton("Cadastrar");
 		btn_cadastrar_adm.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
 		btn_cadastrar_adm.addActionListener(new ActionListener() {
@@ -170,14 +170,14 @@ public class TelaLoginAdm {
 		btn_cadastrar_adm.setFont(new Font("Alef", Font.PLAIN, 25));
 		btn_cadastrar_adm.setBounds(138, 364, 375, 45);
 		panel_1.add(btn_cadastrar_adm);
-		
+
 		txt_senha = new JPasswordField();
 		txt_senha.setForeground(Color.DARK_GRAY);
 		txt_senha.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		txt_senha.setBorder(new LineBorder(new Color(31, 65, 45), 2, true));
 		txt_senha.setBounds(37, 192, 577, 39);
 		panel_1.add(txt_senha);
-		
+
 		JButton btnNewButton = new JButton("<");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -193,11 +193,9 @@ public class TelaLoginAdm {
 		frmLoginDoAdministrador.getContentPane().add(btnNewButton);
 		frmLoginDoAdministrador.setBounds(100, 100, 1600, 850);
 		frmLoginDoAdministrador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
+
 	}
 
-	protected void setVisible(boolean b) {		
+	protected void setVisible(boolean b) {
 	}
 }
