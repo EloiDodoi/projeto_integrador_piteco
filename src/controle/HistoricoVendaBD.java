de9b57bfc047c18dcec514b1fa8dfb577cea5414
eloi <isadora.js23@aluno.ifsc.edu.br>
2022-11-22 11:51:51 -0300

    Historico BD

--- src/controle/HistoricoVendaBD.java
+++ src/controle/HistoricoVendaBD.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
@@ -22,26 +23,23 @@ public class HistoricoVendaBD {
 		jt.setModel(listagemVenda());
 	}
 	
-	public ArrayList<Produto> listaProdutos() {
+	public ArrayList<Object> listaVendas() {
 		try {
-			ArrayList<Venda> venda = new ArrayList<>();
-			ArrayList<String> tipo =  new ArrayList<>();
 			ArrayList<Object> historico = new ArrayList<>();
 			PreparedStatement ps = cbd.getConexao().prepareStatement("SELECT idVenda,venda_data,venda_valor,Tipo_Pagamento FROM venda inner join tipo_pagamento on Tipo_Pagamento_idTipo_Pagamento = idTipo_Pagamento");
 			ResultSet rs = ps.executeQuery();
 	          while(rs.next()){
+	        	  int  i = 0;
 	        	  String tipo_a;
 	        	  Venda v = new Venda();
 	        	  v.setIdVenda(rs.getInt(1));
 	        	  v.setVenda_data(rs.getDate(2));
 	        	  v.setVenda_valor(rs.getFloat(3));
-	        	  venda.add(v);
 	        	  tipo_a = rs.getString(4);
-	        	  tipo.add(tipo_a);
+	        	  historico.add(i, v);
+	        	  historico.add(i, tipo_a);
+	        	  i++;
 	            }
-	          for (Venda v: venda) {
-	        	  
-	          }
 	          return historico;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -50,7 +48,7 @@ public class HistoricoVendaBD {
 	}
 	
 	public DefaultTableModel listagemVenda() {
-		ArrayList<Produto> lp = listaProdutos();
+		ArrayList<Object> lp = listaVendas();
 		DefaultTableModel modelo_tabela = new DefaultTableModel(
 				new Object[][][][][][] {
 					
@@ -60,10 +58,10 @@ public class HistoricoVendaBD {
 				}	
 		);
 		for (int i = 0;i<lp.size();i++) {
-			Produto p = lp.get(i);
-			modelo_tabela.addRow(new Object[] {p.getIdProduto(),p.getProduto_nomeveg(),p.getProduto_especieveg(),p.getProduto_preco(), p.getProduto_quantidade()});
+			Object p = lp.get(i);
+			modelo_tabela.addRow(new Object[] {});
 		}
 		return modelo_tabela;	
-	}
-}
+		}	
+	
 }
